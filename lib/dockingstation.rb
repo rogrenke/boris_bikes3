222df771afc66db347804808de991e3350bb533b
Mário Ribeiro <mail@marioribeiro.com>
2017-05-10 17:05:45 +0100

    Update the bike attribute to bikes and transformed it into an array and added maximum capacity of 20 at the dock method

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -2,15 +2,15 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
-  def initialize(bike = 0)
-    @bike = bike
+  def initialize(bikes = [])
+    @bikes = bikes
   end
   def release_bike
-    fail "no bikes available" unless @bike != 0
-    @bike
+    fail "no bikes available" unless @bikes.any?
+    @bikes.pop
   end
   def dock(bike)
-    fail "station full" unless @bike == 0
-    @bike = bike
+    fail "station full" unless @bikes.length < 20
+    @bikes << bike
   end
 end
