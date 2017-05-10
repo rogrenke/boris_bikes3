bc537c2a8b09ee4d25e92b3c4a22d9863e0a2bd3
jade <jadealvares@hotmail.co.uk>
2017-05-10 13:02:37 +0100

    update DockingStation with bikes attribute and bikes method

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,10 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+  def initialize(bikes = false)
+    @bikes = bikes
+  end
   def release_bike
     Bike.new
   end
   def dock(bike)
-    
+
   end
 end
