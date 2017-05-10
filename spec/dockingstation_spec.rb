6e5f3476a3c6d524e35e294fdbdb7f71c041df34
jade <jadealvares@hotmail.co.uk>
2017-05-10 15:19:14 +0100

    create a test that checks if a bike is released when the docking station is empty

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,7 +4,8 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
   it "releases working bikes" do
-    bike = subject.release_bike
+    station = DockingStation.new(true)
+    bike = station.release_bike
     expect(bike).to be_working
   end
   it "should be able to dock a bike" do
@@ -12,5 +13,9 @@ describe DockingStation do
   end
   it "should be able to know if there is a bike" do
     expect(subject).to respond_to(:bikes)
+  end
+  it "should raise an error when releasing a bike from an empty docking station" do
+    station = DockingStation.new(false)
+    expect{station.release_bike}.to raise_error
   end
 end
