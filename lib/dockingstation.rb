1f30fee3c9ac62bcf6dc3bb0b43a3717cf225871
jade <jadealvares@hotmail.co.uk>
2017-05-10 16:30:34 +0100

    change dock method in DockStation to raise exception if station is full

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -6,10 +6,11 @@ class DockingStation
     @bike = bike
   end
   def release_bike
-    fail "no bikes available" unless @bike
+    fail "no bikes available" unless @bike != 0
     @bike
   end
   def dock(bike)
+    fail "station full" unless @bike == 0
     @bike = bike
   end
 end
