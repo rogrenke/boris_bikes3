1f13c0ac4c0f1f29b52ca524c878f0853c68e03a
“rogrenke” <roland@rolandgrenke.com>
2017-05-11 15:05:18 +0100

    create test to verify that a bike that is reported broken actually is set to not working, change bike.rb accordingly

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,4 +3,9 @@ require "bike"
 describe Bike do
   it { is_expected.to respond_to :working? }
   it { is_expected.to respond_to :report_broken}
+  it "records that a bike is broken if a user reports the bike as broken" do
+    bike = Bike.new
+    bike.report_broken
+    expect(bike.working?).to eq false
+  end
 end
